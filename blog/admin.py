d44d569d42bfdeab362031dc2c012b6216f39195
Miguel-Pires22 <miguel.ap22@outlook.com>
2025-09-23 19:29:17 -0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Author, BlogPost
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'created_at']
+    search_fields = ['name', 'email']
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'featured', 'published', 'created_at']
+    list_filter = ['category', 'featured', 'published', 'created_at']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_editable = ['featured', 'published']
+    date_hierarchy = 'created_at'
+    
+    fieldsets = (
+        ('Informações Básicas', {
+            'fields': ('title', 'slug', 'author', 'category')
+        }),
+        ('Conteúdo', {
+            'fields': ('content', 'excerpt', 'image')
+        }),
+        ('Configurações', {
+            'fields': ('featured', 'published')
+        }),
+    )
